bde4b0217caa654d8a506e0a6e82eb380eaeab0a
eng1mamun <wub.mamun.cse@gmail.com>
2022-02-16 23:09:29 +0600

    First commit

---
+++ EmpProject/Controllers/EMP.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmpProject.Controllers
+{
+    public class EMP : Controller
+    {
+        // GET: EMP
+        [HttpGet]
+        public ActionResult EmployeeEntry()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult EmployeeEntry(string EName, string EDeg, decimal Salary, int Age)
+        //{
+
+        //    return View();
+        //}
+    }
+}
---
+++ EmpProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmpProject.Startup))]
+namespace EmpProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
